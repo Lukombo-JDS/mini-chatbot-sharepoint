0831a79dbffa132ec6a910c1db8965f42e2c5e23
Lukombo-JDS <dos-santos.lukombo@proton.me>
2026-09-08 23:55:52 +0200

    feat: handler constructor created

--- sp-sidecar/internal/transport/httpapi/handler.go
+++ sp-sidecar/internal/transport/httpapi/handler.go
@@ -4,7 +4,20 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"sp-sidecar/internal/application"
+	"sp-sidecar/internal/domain"
 )
+
+
+type DocumentHandler struct {
+    service *application.DocumentSourceService
+}
+
+func NewDocumentHandler(s *application.DocumentSourceService)(*DocumentHandler){
+    return &DocumentHandler{
+        service: s,
+    }
+}
 
 func HealthHandler(w http.ResponseWriter, r *http.Request){
 
@@ -22,5 +35,29 @@ func HealthHandler(w http.ResponseWriter, r *http.Request){
     if errJsonEncoder != nil {
         log.Printf("invalid encoder: %v", errJsonEncoder.Error())
     }
+}
 
+
+func (h *DocumentHandler)ListDocuments(w http.ResponseWriter,r *http.Request){
+    
+    type documentResponse struct{
+        Status string `json:"status"`
+        Documents []domain.Document `json:"documents"`
+    }
+
+    w.Header().Set("content-type", "application/json")
+    w.WriteHeader(http.StatusOK)
+    
+    docs := h.service.ListDocuments()
+    
+    response := documentResponse{
+        Status: "ok",
+        Documents: docs,
+    }
+
+    errJsonEncoder := json.NewEncoder(w).Encode(response)
+    if errJsonEncoder != nil {
+        log.Printf("invalid encoder: %v", errJsonEncoder.Error())
+    }
+    
 }
