56cc8e7b17525d9dbf66b0b86d2abada4cda2f18
Lukombo-JDS <dos-santos.lukombo@proton.me>
2026-09-18 15:41:40 +0200

    refactor(rag): chunker service

--- rag-api/src/rag_api/application/services.py
+++ rag-api/src/rag_api/application/services.py
@@ -3,6 +3,7 @@ from src.rag_api.application.document_source import DocumentSource
 from src.rag_api.domain.models import Chunk, Document, User
 
 
+
 @dataclass
 class DocumentRetrievalService:
     document_source: DocumentSource
@@ -13,49 +14,47 @@ class DocumentRetrievalService:
 @dataclass
 class ChunkingService:
 
-    def chunk_document(self, document: Document, chunk_size:int) -> list[Chunk]:
+    def chunk_document(self, document: Document, chunk_size:int, overlap:int) -> list[Chunk]:
 
-        if chunk_size <=0 :
+        if chunk_size <= 0 :
             raise ValueError("invalid chunk size")
 
+        if overlap < 0 :
+            raise ValueError("invalid overlap value")
+
+        if overlap >= chunk_size:
+            raise ValueError("invalid overlap equal chunk size")
         
-        count_chunk: int = 0
+        end: int = chunk_size
         chunks: list[Chunk] = []
         words: list[str] = document.content.split()
         chunk_content: list[str] = []
+        count_chunk:int = 0
 
-        for wrd in words:
+        start = 0
+        
+        while start < len(words):
 
-            if len(chunk_content) < chunk_size:
-                chunk_content.append(wrd)
-                
-            else:
-                chunks.append(
-                    Chunk(
-                        id=document.id+":chunk-"+str(count_chunk),
-                        document_id=document.id,
-                        chunk_index=count_chunk,
-                        tenant_id=document.tenant_id,
-                        content= " ".join(chunk_content),
-                        allowed_groups=document.allowed_groups,
-                    )
-                )
-                
-                count_chunk += 1
-                chunk_content = []
-                chunk_content.append(wrd)
-                
-        if len(chunk_content) > 0:
+            end = min(start+chunk_size,len(words))
+        
+            chunk_content = words[start:end]
+
             chunks.append(
                 Chunk(
-                    id=document.id+":chunk-"+str(count_chunk),
-                    document_id=document.id,
-                    chunk_index=count_chunk,
-                    tenant_id=document.tenant_id,
-                    content= " ".join(chunk_content),
-                    allowed_groups=document.allowed_groups,
+                    id = document.id + ":chunk-" + str(count_chunk),
+                    document_id = document.id,
+                    chunk_index = count_chunk,
+                    tenant_id = document.tenant_id,
+                    content = " ".join(chunk_content),
+                    allowed_groups = document.allowed_groups,
                 )
             )
+
+            if end == len(words):
+                break
+
+            count_chunk += 1 # increment the id
+            start += (chunk_size-overlap) # sliding window
 
                 
         return chunks
--- rag-api/src/rag_api/application/tests/units/chunk_document.py
+++ rag-api/src/rag_api/application/tests/units/chunk_document.py
@@ -16,42 +16,43 @@ def test_chunk_document():
     )
     
     chunk_size = 3
+    overlap = 2
 
     # Act
-    chunks = ChunkingService().chunk_document(document=fake_document,chunk_size=chunk_size)
-    # print("CHUNKS: ", chunks)
+    chunks = ChunkingService().chunk_document(
+        document=fake_document,
+        chunk_size=chunk_size,
+        overlap=overlap
+    )
+    print("CHUNKS: ", chunks)
 
     content:list[str] = []
 
     # Assert
     for i,chk in enumerate(chunks):
         
-        assert len(chk.content.split()) <= 3
+        assert len(chk.content.split()) <= chunk_size
         assert chk.document_id == fake_document.id
         assert chk.tenant_id == fake_document.tenant_id
         assert chk.allowed_groups == fake_document.allowed_groups
-
+        
         content.append(chk.content)
 
         assert chk.chunk_index == i
         assert chk.id == f"{fake_document.id}:chunk-{i}"
-        
-
-    reconstructed_content = " ".join(content)
-
-    assert reconstructed_content.split() == fake_document.content.split()
 
 
 @pytest.mark.parametrize(
-    "content, chunk_size, expected_chunks",
+    "content, chunk_size,overlap, expected_chunks",
     [
-        ("The One Piece", 5, 1),    # content < chunk_size
-        ("The One Piece", 3, 1),    # content == chunk_size
-        ("The One Piece", 2, 2),    # content > chunk_size
-        ("The One Piece", 1, 3)     # 1 word == chunk_size
+        ("The One Piece", 5, 1, 1),    # content < chunk_size
+        ("The One Piece Live Action is the Best", 8, 1, 1),    # content == chunk_size
+        ("The One Piece", 2, 0, 2),    # content > chunk_size
+        ("The One Piece", 1, 0, 3),     # 1 word == chunk_size
+        ("The One Piece Live Action was made with heart and effort.", 4, 3, 8)
     ]
 )
-def test_chunk_sizes(content, chunk_size, expected_chunks):
+def test_chunk_sizes(content, chunk_size, expected_chunks, overlap):
 
      # Arrange
      fake_document = Document(
@@ -64,8 +65,8 @@ def test_chunk_sizes(content, chunk_size, expected_chunks):
      )
       
      # Act
-     chunks = ChunkingService().chunk_document(document=fake_document,chunk_size=chunk_size)
-     # print("CHUNKS: ", chunks)
+     chunks = ChunkingService().chunk_document(document=fake_document,chunk_size=chunk_size,overlap=overlap)
+     print("CHUNKS: ", chunks)
  
      doc_content:list[str] = []
  
@@ -86,9 +87,9 @@ def test_chunk_sizes(content, chunk_size, expected_chunks):
          assert chk.id == f"{fake_document.id}:chunk-{i}"
          
  
-     reconstructed_content = " ".join(doc_content)
+     # reconstructed_content = " ".join(doc_content)
  
-     assert reconstructed_content.split() == fake_document.content.split()
+     # assert reconstructed_content.split() == fake_document.content.split()
 
 
 def test_chunk_empty_content():
@@ -103,19 +104,23 @@ def test_chunk_empty_content():
         )
         
     chunk_size = 3
+    overlap = 1
 
-    chunks = ChunkingService().chunk_document(document=fake_document, chunk_size=chunk_size)
+    chunks = ChunkingService().chunk_document(document=fake_document, chunk_size=chunk_size,overlap=overlap)
 
     assert chunks == []
 
 @pytest.mark.parametrize(
-    "chunk_size",
+    "chunk_size, overlap, expected_error",
     [
-        (0),
-        (-2)
+        (0,2, "invalid chunk_size"),
+        (2,3, "invalid overlap value"),
+        (-2,1, "invalid chunk size" ),
+        (3,-1, "invalid overlap value"),
+        (3,3, "invalid overlap equal chunk size")
     ]
 )
-def test_zero_negative_chunk_size(chunk_size):
+def test_zero_negative_chunk_size(chunk_size,overlap,expected_error):
 
     fake_document = Document(
             title="One Piece Live Action",
@@ -125,7 +130,60 @@ def test_zero_negative_chunk_size(chunk_size):
             allowed_groups=["risk", "executive"]
             
         )
-    with pytest.raises(ValueError, match="invalid chunk size"):
+        
+    with pytest.raises(ValueError) as invalid_entries:
     
-        _ = ChunkingService().chunk_document(document=fake_document, chunk_size=chunk_size)
+        _ = ChunkingService().chunk_document(
+            document=fake_document, 
+            chunk_size=chunk_size,
+            overlap=overlap
+        )
+
+        assert invalid_entries.match == expected_error
     
+
+@pytest.mark.parametrize(
+    "content, chunk_size, overlap",
+    [
+        ("One Two Three Four Five Six Seven", 3, 1 ),
+        ("One Two Three Four Five Six Seven", 3, 2 ),
+        ("One Two Three Four Five Six Seven", 2, 1 ),
+        ("One Two Three Four Five Six Seven", 4, 1 ),
+        ("One Two Three Four Five Six Seven", 4, 2 ),
+        ("One Two Three Four Five Six Seven", 4, 3 ),
+    ]
+)
+def test_overlap_chunk_content(content, chunk_size, overlap):
+
+    # Arrange
+    fake_document = Document(
+        title="One Piece Live Action",
+        id="doc-1",
+        content=content,
+        tenant_id="Bank-Kara",
+        allowed_groups=["risk", "executive"]
+    )
+    
+
+    # Act
+    chunks = ChunkingService().chunk_document(
+        document = fake_document,
+        chunk_size = chunk_size,
+        overlap = overlap
+    )
+
+    print("CHUNKS: ", chunks)
+
+    # content_doc:list[str] = []
+
+    # Assert
+    for i in range(1,len(chunks)):
+
+      previous_words = chunks[i-1].content.split()
+      current_words = chunks[i].content.split()
+
+      # The last overlap words of previous slice equal
+      # the first overlap words of the current slice  
+      assert previous_words[-overlap:] == current_words[:overlap] 
+
+      
